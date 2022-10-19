0a2ae7e3741eb64440da2c66f71d78f8c3ad30d7
Adrian Morley <adrianmorley95@gmail.com>
2022-10-19 17:10:41 +0100

    added multiple files

---
+++ introtask.py
@@ -0,0 +1,7 @@
+english = int(input("Please enter your Biology marks: "))
+maths = int(input("Please enter your Chemistry marks: "))
+science = int(input("Please enter your Physics marks: "))
+total = english + maths + science
+average = (english + maths + science) / 3
+print("Your total is " + str(total) + ". Your average is " + str(average))
+
---
+++ numeric.py
@@ -0,0 +1,5 @@
+my_int = int(2.6)
+my_float = float(2)
+
+print(my_int)
+print(my_float)
