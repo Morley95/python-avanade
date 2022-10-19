d82f8b455ab8faf6cb4297970b1b8d492eccb227
Adrian Morley <adrianmorley95@gmail.com>
2022-10-19 16:58:41 +0100

    added strings.py

---
+++ strings.py
@@ -0,0 +1,6 @@
+the_fairest = input("Who is the fairest of them all?:")
+
+if the_fairest == "Adrian" or the_fairest == "adrian":
+    print("Correct! Adrian is the fairest of them all")
+else:
+    print("Wrong, Adrian is the fairest of them all")
