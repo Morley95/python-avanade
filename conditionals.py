f06a207ceee0f6938e6aa957869289de01583366
Adrian Morley <adrianmorley95@gmail.com>
2022-10-20 10:40:35 +0100

    added multiple files

---
+++ conditionals.py
@@ -0,0 +1,25 @@
+# score = int(input("What score did you get:"))
+
+# if score >= 60:
+#     print("PASS")
+
+# if score < 60:
+#     print("FAIL")
+
+# score = int(input("What score did you get?:"))
+
+# if score >= 60:
+#     print("PASS")
+# else:
+#     print("FAIL")
+
+# score = int(input("What score did you get?:"))
+
+# if score >= 60:
+#     print("DISTINCTION")
+# elif score >= 40 and score <60:
+#     print("PASS")
+# else:
+#     print("FAIL")
+
+
---
+++ temperaturegauge.py
@@ -0,0 +1,34 @@
+# temperature = 20
+# if (temperature > 30):
+#     print("too hot")
+#     print("aagh")
+#     if (temperature > 50):
+#         print("AAH")
+# print("too cold")
+
+# temperature = -10
+# if (temperature > 30):
+#     print("too hot")
+#     print("aagh")
+# if (temperature < 0):
+#     print("too cold")
+# if (temperature > 0):
+#     print("perfect")
+
+# temperature = 20
+# if (temperature > 30):
+#     print("too hot")
+#     print("aagh")
+# else:
+#     print("AAH")
+# print("too cold")
+
+temperature = 20
+if (temperature > 50):
+    print("very hot")
+    print("AAGH")
+elif (temperature > 30):
+    print("too hot")
+    print("aagh")
+else:
+    print("too cold")
